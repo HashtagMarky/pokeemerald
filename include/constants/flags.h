14475a7b80ebc836e756eeebf8b25e2f8eb1b4db
Phantonomy <131238004+Ddaretrogamer@users.noreply.github.com>
2025-05-14 16:19:52 -0500

    adding EV/IV editor toggled by flag

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1226,7 +1226,7 @@
 #define FLAG_DN_FLAG_SEARCHING                                           0x494 // changed from unused to DN_FLAG_SEARCHING
 #define FLAG_DN_FLAG_DEXNAV_GET                                          0x495 // changed from unused to DN_FLAG_DEXNAV_GET
 #define FLAG_DN_FLAG_DETECTOR_MODE                                       0x496 // changed from unused to DN_FLAG_DETECTOR_MODE
-#define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
+#define FLAG_ENABLE_STAT_EDITOR                                           0x497 // Unused Flag changed to stat editor
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
 #define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
 #define FLAG_UNUSED_0x49A                                           0x49A // Unused Flag

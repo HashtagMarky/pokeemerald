033d322080c59b1f7e896c7e9d73e1efe559eca6
Phantonomy <131238004+Ddaretrogamer@users.noreply.github.com>
2025-07-04 11:07:22 -0500

    Updating OW mons spawns and adding sparkle effect

--- include/constants/field_effects.h
+++ include/constants/field_effects.h
@@ -125,6 +125,7 @@
 #define FLDEFFOBJ_TRACKS_SPOT           38
 #define FLDEFFOBJ_TRACKS_BUG            39
 #define FLDEFFOBJ_CAVE_DUST             40
+#define FLDEFFOBJ_SHINY_SPARKLE         41
 
 #define FLDEFF_PAL_TAG_CUT_GRASS          0x1000
 #define FLDEFF_PAL_TAG_SECRET_POWER_TREE  0x1003
